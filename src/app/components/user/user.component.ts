9e493e596fa424c1f5a5a4c975ddc63ce7c5c4f4
Vishesh <visheshsingh16@gmail.com>
2018-10-03 14:11:53 -0400

    users component via cli

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,24 @@
 import { Component } from "@angular/core";
+import { User } from "../../models/User";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
-export class UserComponent {}
+export class UserComponent {
+  user: User;
+
+  constructor() {
+    this.user = {
+      firstName: "Vishesh",
+      lastName: "Thakur",
+      age: 25,
+      address: {
+        street: "123 Heaven Street",
+        city: "New York",
+        state: "NY"
+      }
+    };
+  }
+}
